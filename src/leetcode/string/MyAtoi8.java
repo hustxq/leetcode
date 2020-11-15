f8a889222393713ab8ff06fe9f97e38fa0f1ba56
hustxq <26315874@qq.com>
2020-11-15 21:58:59 +0800

    1115

---
+++ src/leetcode/string/MyAtoi8.java
@@ -0,0 +1,12 @@
+package leetcode.string;
+
+public class MyAtoi8 {
+
+    static public int myAtoi(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        s = s.trim();
+        return 0;
+    }
+}
